6bf237cf982b25a2d4b2597c3f0fd1a73ddc62c7
Baekjoon Choi <choi@startlink.io>
2016-05-23 16:44:17 +0900

    Initial Commit

---
+++ ChosungConvert/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ChosungConvert
+//
+//  Created by Baekjoon Choi on 5/23/16.
+//  Copyright © 2016 Megalusion. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
