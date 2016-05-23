a5b13b13a808859cb0424dfe8829d26a2b11f6f9
Baekjoon Choi <choi@startlink.io>
2016-05-23 17:49:28 +0900

    Update a lot of things

--- ChosungConvert/main.m
+++ ChosungConvert/main.m
@@ -27,30 +27,49 @@ int main(int argc, const char * argv[]) {
         // 19 * 21 * 28
         // 0xAC00 + 21*28*cho + 28*jung + jong = 최
         NSArray *chosungs = @[@"ㄱ", @"ㄲ", @"ㄴ", @"ㄷ", @"ㄸ", @"ㄹ", @"ㅁ", @"ㅂ", @"ㅃ", @"ㅅ", @"ㅆ", @"ㅇ", @"ㅈ", @"ㅉ", @"ㅊ", @"ㅋ", @"ㅌ", @"ㅍ", @"ㅎ"];
-        NSString *name = @"최백준";
+        
+        NSString *name = @"안녕하세요. Hello! HiHiㅎㅇㅎㅇ하이 배가고파요ㅠㅠ";
         NSLog(@"%@",name);
         NSMutableString *result = [NSMutableString string];
         for (NSUInteger i=0; i<[name length]; i++) {
             unichar c = [name characterAtIndex:i];
-            NSLog(@"%tu %C %zd", i, c, (NSInteger)c);
-            NSInteger x = (NSInteger)c - 0xAC00;
-            NSLog(@"%zd 번째", x);
-            // 21*28*cho + 28*jung + jong = x
-            // 28*(21*cho + jung) + jong = x
-            NSInteger jong = x % 28;
-            // 28*(21*cho + jung) = x
-            x = x - jong;
-            // 21*cho + jung = x
-            x = x / 28;
-            NSInteger jung = x % 21;
-            x -= jung;
-            // 21*cho = x
-            NSInteger cho = x / 21;
-            NSString *chosung = chosungs[cho];
-            NSLog(@"%C %zd 번째 초성 %@", c, cho, chosung);
-            [result appendString:chosung];
+            if (0xAC00 <= c && c <= 0xD7AF) {
+                NSLog(@"%tu %C %zd", i, c, (NSInteger)c);
+                NSInteger x = (NSInteger)c - 0xAC00;
+                NSLog(@"%zd 번째", x);
+                // 21*28*cho + 28*jung + jong = x
+                // 28*(21*cho + jung) + jong = x
+                NSInteger jong = x % 28;
+                // 28*(21*cho + jung) = x
+                x = x - jong;
+                // 21*cho + jung = x
+                x = x / 28;
+                NSInteger jung = x % 21;
+                x -= jung;
+                // 21*cho = x
+                NSInteger cho = x / 21;
+                NSString *chosung = chosungs[cho];
+                NSLog(@"%C %zd 번째 초성 %@", c, cho, chosung);
+                [result appendString:chosung];
+            } else {
+                NSString *letter = [name substringWithRange:NSMakeRange(i, 1)];
+                NSLog(@"예외: %@", letter);
+                NSUInteger index = [chosungs indexOfObject:letter];
+                NSLog(@"인덱스: %tu", index);
+                if (index != NSNotFound) {
+                    [result appendString:letter];
+                }
+            }
         }
+        NSString *converted = result;
+        converted = [converted stringByReplacingOccurrencesOfString:@"ㄱㄱ" withString:@"ㄲ"];
+        converted = [converted stringByReplacingOccurrencesOfString:@"ㄷㄷ" withString:@"ㄸ"];
+        converted = [converted stringByReplacingOccurrencesOfString:@"ㅂㅂ" withString:@"ㅃ"];
+        converted = [converted stringByReplacingOccurrencesOfString:@"ㅅㅅ" withString:@"ㅆ"];
+        converted = [converted stringByReplacingOccurrencesOfString:@"ㅈㅈ" withString:@"ㅉ"];
+        
         NSLog(@"%@",result);
+        NSLog(@"%@",converted);
     }
     return 0;
 }
